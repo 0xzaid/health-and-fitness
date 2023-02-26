86fa741814daaa6a930b27b8692ef3f3696e4aea
0xzaid <zaiddirb@gmail.com>
2023-02-27 00:01:25 +0800

    initial nutrition tracker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,6 +37,13 @@ export default function Home() {
               Browse through my personal weekly workouts that I follow and update everyday.
             </p>
           </Link>
+
+          <Link href="#" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+            <h3 className="text-2xl font-bold justify-between">Nutrition Tracker &rarr;</h3>
+            <p className="mt-4 text-xl text-justify">
+              Nutrition tracker to keep count of your daily calories and macros.
+            </p>
+          </Link>
           
 
         </div>
