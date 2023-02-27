ab8a37a4505e2be37148127789707fc8fcf2d941
0xzaid <zaiddirb@gmail.com>
2023-02-27 19:17:45 +0800

    fixed white text not showing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         {!loading && (
-          <h1 className="text-6xl font-bold">
+          <h1 className="text-white text-6xl font-bold">
             Welcome to My Health & Fitness website
           </h1>
         )}
@@ -49,33 +49,33 @@ export default function Home() {
         {!loading && (
           <>
             {user ? (
-              <p className="mt-3 text-xl">
+              <p className="text-white mt-3 text-xl">
                 Welcome {user.displayName}!
               </p>
             ) : (
-              <p className="mt-3 text-xl">
+                <p className="text-white mt-3 text-xl">
                   Please <Link className="text-blue-700 hover:text-blue-400" href="/auth">login</Link> through Google!
               </p>
             )}
 
             <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
               <Link href="/calculators" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-                <h3 className="text-2xl font-bold justify-between">Calculators &rarr;</h3>
-                <p className="mt-4 text-xl text-justify">
+                <h3 className="text-white text-2xl font-bold justify-between">Calculators &rarr;</h3>
+                <p className="text-white mt-4 text-xl text-justify">
                   Browse through our collection of calculators such as BMI, BMR, and more.
                 </p>
               </Link>
 
               <Link href="https://sprinkle-iris-307.notion.site/Exercise-29775913962a423bbf2e4a189a39923b" target="_blank" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-                <h3 className="text-2xl font-bold justify-between">My Workouts &rarr;</h3>
-                <p className="mt-4 text-xl text-justify">
+                <h3 className="text-white text-2xl font-bold justify-between">My Workouts &rarr;</h3>
+                <p className="text-white mt-4 text-xl text-justify">
                   Browse through my personal weekly workouts that I follow and update everyday.
                 </p>
               </Link>
 
               <Link href="#" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-                <h3 className="text-2xl font-bold justify-between">Nutrition Tracker &rarr;</h3>
-                <p className="mt-4 text-xl text-justify">
+                <h3 className="text-white text-2xl font-bold justify-between">Nutrition Tracker &rarr;</h3>
+                <p className="text-white mt-4 text-xl text-justify">
                   Nutrition tracker to keep count of your daily calories and macros.
                 </p>
               </Link>
