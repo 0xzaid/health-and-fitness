caa6e8ed4bf28a7f57019822d0170653f188421a
0xzaid <zaiddirb@gmail.com>
2023-03-01 17:20:10 +0800

    added img to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import firebase from "../../firebase/clientApp"
+import Image from 'next/image';
 
 
 const navLinkVariants = {
@@ -36,8 +37,9 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex-shrink-0">
-                        <Link href="/" className="font-bold text-xl">
-                            Health
+                        <Link href="/" className="flex items-center">
+                            <Image src="/icons/dumbbell.svg" className="h-6 sm:h-12" alt="Health Logo" width="64" height="32" quality="100" />
+                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Health</span>
                         </Link>
                     </div>
                     <div className="hidden md:block">
